f681e8a9eed475653d0c29afcbd7d5d4e955be8b
HenriqueCalazanss <henriquecallazans@gmail.com>
2024-08-16 22:40:24 -0300

    1

--- netlify/functions/writeData.js
+++ netlify/functions/writeData.js
@@ -1,72 +1,62 @@
 const { initializeApp } = require('firebase/app');
-const { getDatabase, ref, set, remove, get, update } = require('firebase/database');
+const { getDatabase, ref, set, get, child } = require('firebase/database');
 
 const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "csbet-00001.firebaseapp.com",
-    projectId: "csbet-00001",
-    storageBucket: "csbet-00001.appspot.com",
-    messagingSenderId: "379404731204",
-    appId: "1:379404731204:web:94eb3e29df6c0a6c214d11",
-    databaseURL: "https://csbet-00001-default-rtdb.firebaseio.com/"
+  apiKey: "API_KEY",
+  authDomain: "csbet-00001.firebaseapp.com",
+  projectId: "csbet-00001",
+  storageBucket: "csbet-00001.appspot.com",
+  messagingSenderId: "379404731204",
+  appId: "1:379404731204:web:94eb3e29df6c0a6c214d11",
+  databaseURL: "https://csbet-00001-default-rtdb.firebaseio.com/"
 };
 
 // Inicializar o Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+initializeApp(firebaseConfig);
+const database = getDatabase();
 
-exports.handler = async (event) => {
-    if (event.httpMethod !== 'POST') {
-        return {
-            statusCode: 405,
-            body: 'Método não permitido'
-        };
-    }
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Método não permitido'
+    };
+  }
 
-    let body;
+  let body;
 
-    try {
-        body = JSON.parse(event.body);
-    } catch (err) {
-        return {
-            statusCode: 400,
-            body: 'Corpo da requisição inválido'
-        };
-    }
+  try {
+    body = JSON.parse(event.body);
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: 'Corpo da requisição inválido'
+    };
+  }
 
-    const { action, path, data } = body;
+  const { path, data } = body;
 
-    try {
-        switch (action) {
-            case 'set':
-                await set(ref(database, path), data);
-                break;
-            case 'remove':
-                await remove(ref(database, path));
-                break;
-            case 'update':
-                await update(ref(database, path), data);
-                break;
-            case 'get':
-                const snapshot = await get(ref(database, path));
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify(snapshot.val())
-                };
-            default:
-                return {
-                    statusCode: 400,
-                    body: 'Ação inválida'
-                };
-        }
-        return {
-            statusCode: 200,
-            body: 'Operação realizada com sucesso!'
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: 'Erro ao realizar operação: ' + error.message
-        };
-    }
+  try {
+    const testPath = 'testConnection';
+    const testData = { message: 'Conexão com Firebase bem-sucedida!' };
+    await set(ref(database, testPath), testData);
+
+    const snapshot = await get(child(ref(database), testPath));
+    const testMessage = snapshot.val();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        message: 'Dados gravados com sucesso!',
+        testMessage
+      })
+    };
+  } catch (error) {
+    console.error('Erro ao gravar dados:', error.message);
+    return {
+      statusCode: 500,
+      body: 'Erro ao gravar dados: ' + error.message
+    };
+  }
 };
